ef5efecd5a1ae27661273e2e8648fee198503aa2
somekindofwallflower <someone.ana13@gmail.com>
2021-10-05 20:21:21 +0200

    feat: Values and Variables

---
+++ 01-Fundamentals-Part-1/02_values_and_variables.js
@@ -0,0 +1,45 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matlda = 'JM';
+console.log(jonas_matlda);
+let $function = 27;
+console.log($function);
+
+let person = "jonas";
+console.log(person);
+let PI = 3.1415;
+console.log(PI);
+
+let myFirstJob = "Programmer";
+console.log(myFirstJob);
+let myCurrentJob = "Teacher";
+console.log(myCurrentJob);
+
+let job1 = "Programmer";
+console.log(job1);
+let job2 = "Teacher";
+console.log(job2);
+
+
+// ***************  NOTES: VALUES AND VARIABLES  ************************
+
+// 1- Variables name should not start with a number let 3name = "Wallflower" is going to through a syntax error
+
+// 2- Variable names can contain letters, numbers, underscores or $ (dollar) sign if I try to declare a variable name first&name is going to through an error on the console
+
+// 3- Variable names should not be same to reserved language words for example <i>new</i> or <i>function</i>.
+
+// 4- Variable names should not start with uppercase letter let Person, usually this syntax is being used for classes in JS
+
+// 5- Constants should be written in uppercase, so it is easy to understand that the value store is not going to change. let PI = 3.14
+
+// 6- Variables name should be descriptive
