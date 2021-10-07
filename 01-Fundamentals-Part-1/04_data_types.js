2e544087bd2fa35dbc14a84927ff970048c9308c
somekindofwallflower <someone.ana13@gmail.com>
2021-10-07 19:11:31 +0200

    feat : Javascript data types - Explained in details primitive data types

---
+++ 01-Fundamentals-Part-1/04_data_types.js
@@ -0,0 +1,55 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+let john;
+console.log(typeof john);
+console.log(john);
+
+let mary = null;
+console.log(typeof mary);
+console.log(mary);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//Javascript bug that has not been solved for legacy reasons - returns object
+console.log(typeof null);
+
+
+// **************** NOTES: DATA TYPES *******************
+
+// In Javascript every value is either an object or a primitive value
+
+/* Primitive data types
+1- Number: Floating point numbers - Used for decimal integers let age = 23
+
+2- String: Sequence of characters - Used for text
+
+3- Boolean: Logical type that can be only true or false - Used for taking decisions
+
+4- Undefined: Value taken by a variable that us nit yet defined (empty value)
+
+5- Null: Also means empty value
+
+6- Symbol: Value that is unique and can not be changed
+
+7- BigInt: Larger integers than the number type can hold */
+
+// Javascript has dynamic typing: We do not have to manually define the data of the value stored in a variable. Instead, data types are determined automatically.
+
+
+
+
