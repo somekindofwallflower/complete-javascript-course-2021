3987557901d21a91c97e3bd4754ca55ed66df7a7
somekindofwallflower <someone.ana13@gmail.com>
2021-10-15 22:40:03 +0200

    feat: Boolean logic

---
+++ 01-Fundamentals-Part-1/15_boolean_logic.js
@@ -0,0 +1,22 @@
+//Boolean logic
+
+const age = 16;
+
+const a = age >= 20; //false
+const b = age < 30; //true
+
+// NOT operator
+
+console.log(!a); //true
+
+// AND operator
+
+console.log(a && b); //false
+
+console.log(!a && b); //true
+
+// OR operator
+
+console.log( a || b); //true
+
+console.log (a || !b); //false
