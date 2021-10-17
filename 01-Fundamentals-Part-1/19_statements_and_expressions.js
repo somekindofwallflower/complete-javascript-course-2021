aa57a4d754af7bcc356c059a49044e5ec7f44fa8
somekindofwallflower <someone.ana13@gmail.com>
2021-10-17 15:33:17 +0200

    feat: Statements and expressions

---
+++ 01-Fundamentals-Part-1/19_statements_and_expressions.js
@@ -0,0 +1,15 @@
+// Statements and expressions
+
+// An expression is a piece of code that produces value
+3 + 4;
+1991;
+true && false && !false;
+
+// Statement is a bigger piece of code that is executed and which does not produce a value on itself.
+
+if (23 > 10) {
+    const str = "23 is bigger";
+}
+
+const me = 'Jonas';
+console.log(` I'm ${2037 - 1991} years old ${me}`)
