4aa1d985ea1010323da2f979b8a5fc9fcbe7d290
somekindofwallflower <someone.ana13@gmail.com>
2021-10-19 22:55:27 +0200

    feat: Functions in Javascript

---
+++ 02-Fundamentals-Part-2/02_functions.js
@@ -0,0 +1,20 @@
+// Functions
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges}`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
