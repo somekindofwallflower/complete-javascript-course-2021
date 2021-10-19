531d2ca03b2e7a71a2059b69c1513fdb5accf337
somekindofwallflower <someone.ana13@gmail.com>
2021-10-19 22:33:24 +0200

    feat: Usage and advantages od using Strict mode

---
+++ 02-Fundamentals-Part-2/01_strict_mode.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// Enable strict mode to check the problem on the line above
+// if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive :D");
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
