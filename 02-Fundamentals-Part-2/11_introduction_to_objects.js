8d9d0ec659306ab819d8b200b33a5ccbe5d03281
somekindofwallflower <someone.ana13@gmail.com>
2021-10-25 11:51:31 +0200

    feat: Introduction to objects

---
+++ 02-Fundamentals-Part-2/11_introduction_to_objects.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037-1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+console.log(typeof jonasArray);
+
+const jonasObject = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037-1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+};
+
+console.log('jonasArray', jonasArray);
+console.log('jonasObject', jonasObject);
