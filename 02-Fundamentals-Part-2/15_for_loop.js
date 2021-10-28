eb6118f29d91202c2f3e0fa6c80c5e274b2af94e
somekindofwallflower <someone.ana13@gmail.com>
2021-10-28 19:27:34 +0200

    feat: For loops

---
+++ 02-Fundamentals-Part-2/15_for_loop.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// For loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`)
+}
