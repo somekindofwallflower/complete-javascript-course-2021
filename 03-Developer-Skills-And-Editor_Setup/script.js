b86d2d8f5734ef9b2005f38cbf1a24f48366cdbc
somekindofwallflower <someone.ana13@gmail.com>
2021-11-04 22:17:15 +0100

    Setting up prettier

--- 03-Developer-Skills-And-Editor_Setup/script.js
+++ 03-Developer-Skills-And-Editor_Setup/script.js
@@ -1,2 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+const x = "23";
+
+// Condition is always false since types 'string' and 'number' have no overlap
+if (x === 23) {
+  console.log(23);
+}
+
+const calcAge = (birthYear) => 2037 - birthYear;
