0c08a7ff02a061f08139c71f92186c68230c6624
somekindofwallflower <someone.ana13@gmail.com>
2021-12-08 22:47:56 +0100

    Manipulate the css style using querySelector  in case user manage to guess the right number

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -29,10 +29,17 @@ document.querySelector('.number').textContent = secretNumber;
 // Handle click event on button click
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
+
+    // When there is no input
     if (!guess) {
+
         document.querySelector('.message').textContent = "⛔️ No number!";
+    // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!";
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+   // When guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = "📈 Too high!";
@@ -42,6 +49,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = "💥 You lost the game!";
             document.querySelector('.score').textContent = 0;
         }
+
+    // When guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = "📉 Too low!";
