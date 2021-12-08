ad04ae158b0b90ad2a5c57760338a37da5b28a93
somekindofwallflower <someone.ana13@gmail.com>
2021-12-08 20:15:50 +0100

    Selecting and manipulating html elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log(document.querySelector('.message').textContent);
+//Read the content of a html element
+document.querySelector('.message').textContent;
+// Change the content of a html element
+document.querySelector('.message').textContent = 'Correct number';
+// Read the updated content
+document.querySelector('.message').textContent;
+// Manipulate the content of html element with class .number
+document.querySelector('.number').textContent = 13;
+// Manipulate the content of html element with class .score
+document.querySelector('.score').textContent = 20;
+// Read the content of html element with class .guess
+document.querySelector('.guess').value;
+// Manipulate the content of html element with class .guess
+document.querySelector('.guess').value = 20;
+
