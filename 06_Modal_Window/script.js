5a32c04bba00e340ca8dba74a55bcf2e33ef4750
somekindofwallflower <someone.ana13@gmail.com>
2021-12-11 00:52:56 +0100

    Handle esc keypress event

--- 06_Modal_Window/script.js
+++ 06_Modal_Window/script.js
@@ -21,3 +21,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+})
+
