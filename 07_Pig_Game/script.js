0e8364e3244738a2dc400fda1afbc07d5a3ce7ba
somekindofwallflower <someone.ana13@gmail.com>
2021-12-17 15:45:15 +0100

    Implement rolling the dice logic

--- 07_Pig_Game/script.js
+++ 07_Pig_Game/script.js
@@ -4,9 +4,33 @@
 const score0El = document.querySelector('#score--0');
 // Note: getElementById works a bit faster than querySelector
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+//Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function() {
+    // 1. Generate a random dice roll
+    const dice  = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice != 1) {
+        // Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE  LATER
+    } else {
+        // Switch to next player
+    }
+})
 
