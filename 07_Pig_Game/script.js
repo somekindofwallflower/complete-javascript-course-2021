2e73199a25caa7cdd496067daa9ee18df83c0815
somekindofwallflower <someone.ana13@gmail.com>
2021-12-17 13:36:09 +0100

    When game is not started yes set players score to 0 and hide the dice

--- 07_Pig_Game/script.js
+++ 07_Pig_Game/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+// Note: getElementById works a bit faster than querySelector
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
