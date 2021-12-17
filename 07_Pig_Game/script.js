1cdbb1b4cfb2fce3161254f4e8121d7f04124bbe
somekindofwallflower <someone.ana13@gmail.com>
2021-12-17 16:43:14 +0100

    Switch the active player

--- 07_Pig_Game/script.js
+++ 07_Pig_Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 // Note: getElementById works a bit faster than querySelector
 const score1El = document.getElementById('score--1');
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function() {
     // 1. Generate a random dice roll
@@ -28,9 +32,14 @@ btnRoll.addEventListener('click', function() {
     if (dice != 1) {
         // Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore; // CHANGE  LATER
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
 
