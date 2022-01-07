4c1c13d7d32bce6e6834de288de10797c7dbce22
somekindofwallflower <someone.ana13@gmail.com>
2022-01-07 15:39:58 +0100

    Three small examples of enhanced object literals

---
+++ 09_Data_Structures_Modern_Operators_and_Strings/10_enhanced_object_literals.js
@@ -0,0 +1,37 @@
+'use strict';
+const weekDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+const openingHours = {
+    [weekDays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekDays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekDays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+}
+
+// Data needed for a later exercise
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    // ES6 enhanced object literals
+    openingHours,
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+};
+
+
+console.log('restaurant', restaurant);
