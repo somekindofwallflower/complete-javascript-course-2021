d9642fe8bbfb3ed9905c20ff457d7c848b80ff15
somekindofwallflower <someone.ana13@gmail.com>
2022-01-08 23:45:16 +0100

    Maps iteration

--- 09_Data_Structures_Modern_Operators_and_Strings/15_maps.js
+++ 09_Data_Structures_Modern_Operators_and_Strings/15_maps.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const weekDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+const openingHours = {
+    [weekDays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekDays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekDays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+}
 // Map is a data structure that we use to map values to keys - keys can be any type
 
 // 1. Create a map
@@ -48,3 +63,37 @@ rest.set(document.querySelector('h1'), "heading");
 console.log(rest);
 console.log(rest.size);
 console.log(rest.get(arr));
+
+
+// ------- Populate map - second way
+const question = new Map(
+    [
+        ["question", "What is the best programming language in the world?"],
+        ["1", "C"],
+        ["2", "Java"],
+        ["3", "JavaScript"],
+        ["correct", 3],
+        [true, "Correct"],
+        [false, "Try again!"],
+    ]
+);
+console.log(question);
+// Covert object to map
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"))
+for(const [key, value] of question) {
+    if(typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+console.log(question.get(question.get("correct") === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
